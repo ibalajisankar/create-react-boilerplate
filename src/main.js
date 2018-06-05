164305ceca1d697d77642ab2cbfc30938d1444b9
mahe94 <mahesh9415@gmail.com>
2018-06-05 15:25:36 +0530

    Boilerplate with ant design UI kit, less, sass, axios, react router and linting                     over ride colors in src/less/var.css

--- src/main.js
+++ src/main.js
@@ -1,14 +1,23 @@
 import React, {Component} from 'react';
-import {Route, Switch} from 'react-router-dom';
-import Sample from './sample';
+import { Route, Switch } from 'react-router-dom';
+import  Sample  from './sample'
+// import official less entry file
+
 
 export default class Main extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        };
+    }
+    
     render() {
         return (
             <main>
                 <Switch>
                     <Route exact path='/' component={Sample}/>
-
                 </Switch>
             </main>
         )
---
+++ src/sample/index.jsx
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+import logo from '../logo.svg';
+import { Button } from 'antd';
+
+class Sample extends Component {
+    render() {
+        return (
+            <div className="App">
+            <div className="wrap">
+                <h1 className="hello">
+                Boilerplate with ant design UI kit, less, sass, axios, react router and linting
+                    over ride colors in src/less/var.css       
+                </h1>
+                <Button type="primary">Primary</Button>
+                <Button>Default</Button>
+                <Button type="dashed">Dashed</Button>
+                <Button type="danger">Danger</Button>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Sample;
